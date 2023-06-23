d3e59bd24285769be52f04df11b54cc82949bd1d
qara-qurt <serikov.2002.12@gmail.com>
2023-06-23 16:57:43 +0600

    auth:added gracefulShutdown

--- auth/cmd/main.go
+++ auth/cmd/main.go
@@ -6,7 +6,13 @@ import (
 	"auth/internal/service"
 	"auth/internal/transport/http"
 	"auth/internal/transport/http/handler"
+	"context"
 	"github.com/sirupsen/logrus"
+	"log"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -31,9 +37,34 @@ func run() error {
 	srv := http.NewServer(config, handler)
 	srv.InitRouter()
 	logrus.Info("routes successfully added")
-	if err := srv.Run(); err != nil {
-		logrus.Error()
-		return err
-	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := srv.Run(); err != nil {
+			logrus.Error(err)
+			logrus.Fatal(err)
+		}
+	}()
+	gracefulShutdown(srv, wg)
+
+	wg.Wait()
 	return nil
+}
+
+func gracefulShutdown(srv *http.Server, wg sync.WaitGroup) {
+	defer wg.Done()
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	<-ctx.Done()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.HTTP.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+	log.Println("Server stopped gracefully")
 }
